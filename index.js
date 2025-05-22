9d38503e5e65ab634fab850db6d0f9ea10638534
Sowmitra Guha <sowmmochy@gmail.com>
2025-05-22 16:17:00 +0600

    data updated and get from database

--- index.js
+++ index.js
@@ -23,12 +23,35 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const coffeesCollection = client.db('coffeeDB').collection('coffees');
+        // coffeesCollection.updateMany(
+        //     {},
+        //     { $rename: { "quantity": "price" } }
+        // )
+
+        //Get coffee data from db
+        app.get('/coffees', async (req, res) => {
+            const result = await coffeesCollection.find().toArray();
+            res.send(result);
+        })
+
+        //add coffee to db
+        app.post('/coffees', async (req, res) => {
+            const newCoffee = req.body;
+            console.log(newCoffee);
+            const result = await coffeesCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
